53bbe1dcf0e40862ef13dadc461daf1a29cd5256
fatemehchahardoli <chahardolifatemehb@gmail.com>
2024-02-23 20:00:44 +0330

    component main

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import Header from '../../components/Header/Header';
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
-  return (
-    <>
-    <Navbar/>
-    <Header/>
-    </>
-  )
-}
+   return (
+      <>
+         <Navbar />
+      </>
+   );
+};
 
 export default Home;
