3054e5f5cb63ad0bac592bc98d2b0b2ad75a4d0d
fatemehchahardoli <chahardolifatemehb@gmail.com>
2024-02-28 17:18:02 +0330

    details

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,22 +1,24 @@
-import React from "react";
+// import React from "react";
+// import { useParams } from "react-router-dom";
+// import useFetch from "../../hooks/useFetch";
+// const Detail = (id, image, status, species, location) => {
+//    return (
+//       <section className="min-h-screen">
+//          {console.log(id)}
+//          {/* image character */}
+//          <div className="mx-auto flex min-h-screen max-w-screen-sm pt-14 justify-center">
+//             <div className="h-96 w-[60rem] rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+//                <div className="flex h-full w-full items-center justify-center bg-gray-800 back">
+//                   <img
+//                      src="images/main/MV5BZjRjOTFkOTktZWUzMi00YzMyLThk (1).jpg"
+//                      className="object-cover"
+//                      alt="image"
+//                   />
+//                </div>
+//             </div>
+//          </div>
+//       </section>
+//    );
+// };
 
-const Detail = () => {
-   return (
-      <section className="min-h-screen">
-         {/* image character */}
-         <div className="mx-auto flex min-h-screen max-w-screen-sm pt-14 justify-center">
-            <div className="h-96 w-[60rem] rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
-               <div className="flex h-full w-full items-center justify-center bg-gray-800 back">
-                  <img
-                     src="images/main/MV5BZjRjOTFkOTktZWUzMi00YzMyLThk (1).jpg"
-                     className="object-cover"
-                     alt="image"
-                  />
-               </div>
-            </div>
-         </div>
-      </section>
-   );
-};
-
-export default Detail;
+// export default Detail;
--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,15 +1,24 @@
-import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer";
-import Detail from "../../components/Detail/Detail";
-const Details = () => {
-   return (
-      <>
-         <Navbar />
-         <Detail />
-         <Footer />
-      </>
-   );
-};
+// import React, { useState } from "react";
+// import Navbar from "../../components/Navbar/Navbar";
+// import Footer from "../../components/Footer/Footer";
+// import Detail from "../../components/Detail/Detail";
+// import useFetch from "../../hooks/useFetch";
+// import { useParams } from "react-router-dom";
+// const Details = () => {
+//   const { characters,locations,episodes}=useFetch();
+//   const [character,setCharacter]=useState(null) ;
+//      setCharacter(characters.results.filter((ch)=>{
+//            return ch.id===params.characterID;
+//         })) 
+//   let params=useParams();
+//    return (
+//       <>
+//          <Navbar />
+        
+//          {/* <Detail {...character} /> */}
+//          <Footer />
+//       </>
+//    );
+// };
 
-export default Details;
+// export default Details;
