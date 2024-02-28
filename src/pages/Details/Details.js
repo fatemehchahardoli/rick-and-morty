d7257ec5c6100afcffba5a77a45763fe6e5f028a
fatemehchahardoli <chahardolifatemehb@gmail.com>
2024-02-28 23:40:19 +0330

    clean code

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-import Detail from "../../components/Detail/Detail";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 const Details = () => {
